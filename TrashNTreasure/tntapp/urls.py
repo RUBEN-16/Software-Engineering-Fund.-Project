d54d5320d915ed58f1dddd29245b0454e02282ad
Ruben <rubeneswaranramalingam@gmail.com>
2024-11-10 16:05:29 +0800

    Change TO flaskk

--- TrashNTreasure/tntapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('start/', views.page, name='Page'),
-    path('login/', views.log, name = 'Login'),
-    path('register/', views.registration, name = 'Register'),
-]
-
