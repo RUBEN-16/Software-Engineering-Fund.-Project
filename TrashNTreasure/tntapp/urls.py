3a35bfa80b26d738e434d92aa63d805141650aa8
Ruben <rubeneswaranramalingam@gmail.com>
2024-11-10 14:08:14 +0800

    connected

--- TrashNTreasure/tntapp/urls.py
+++ TrashNTreasure/tntapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('start/', views.page),
+    path('start/', views.page, name='Page'),
+    path('login/', views.log, name = 'Login'),
+    path('register/', views.registration, name = 'Register'),
 ]
 
--- TrashNTreasure/tntapp/views.py
+++ TrashNTreasure/tntapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def page(request):
     return render(request, 'home.html')
+
+def log(request):
+    return render(request, 'login.html')
+
+def registration(request):
+    return render(request, 'register.html')
